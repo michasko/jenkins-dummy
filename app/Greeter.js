9ef6ac3a69d31077ee5005711a2cc62b88037cf7
Michał Leszczyk <michal.leszczyk@performgroup.com>
2018-07-18 13:30:11 +0200

    feat: custom greeting

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,21 +1,22 @@
 const { count } = require('./lib/util')
 
 class Greeter {
-	constructor(names) {
+	constructor(names, greeting = 'Hello') {
 		this.guests = Array.isArray(names) ? names : [ names ]
+		this.greeting = greeting
 	}
 
 	addGuest(name) {
 		this.guests.push(name)
 	}
 
-	get greeting() {
+	greet() {
 		if (count(this.guests) > 2) {
-			return `Hello ${this.guests.slice(0, -1).join(', ')} and ${this.guests.slice(-1)}!`
+			return `${this.greeting} ${this.guests.slice(0, -1).join(', ')} and ${this.guests.slice(-1)}!`
 		} else if (count(this.guests) > 1) {
-			return `Hello ${this.guests[0]} and ${this.guests[1]}!`
+			return `${this.greeting} ${this.guests[0]} and ${this.guests[1]}!`
 		}
-		return `Hello ${this.guests[0]}!`
+		return `${this.greeting} ${this.guests[0]}!`
 	}
 }
 
