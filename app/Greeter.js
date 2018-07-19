ee1ef592eafc5bd61970bde98cd980d63de98fff
Michał Leszczyk <michal.leszczyk@performgroup.com>
2018-07-19 07:47:09 +0200

    feature: count guests

--- app/Greeter.js
+++ app/Greeter.js
@@ -10,12 +10,17 @@ class Greeter {
 	}
 
 	get greeting() {
+		let greeting
+
 		if (count(this.guests) > 2) {
-			return `Hello ${this.guests.slice(0, -1).join(', ')} and ${this.guests.slice(-1)}!`
+			greeting = `Hello ${this.guests.slice(0, -1).join(', ')} and ${this.guests.slice(-1)}!`
 		} else if (count(this.guests) > 1) {
-			return `Hello ${this.guests[0]} and ${this.guests[1]}!`
+			greeting = `Hello ${this.guests[0]} and ${this.guests[1]}!`
+		} else {
+			greeting = `Hello ${this.guests[0]}!`
 		}
-		return `Hello ${this.guests[0]}!`
+
+		return `${greeting} We've got ${count(this.guests)} guests total!`
 	}
 }
 
