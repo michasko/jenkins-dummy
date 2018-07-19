0e2dda5d83ca8ab7cb482912cab39dd2f6621fb4
Michał Leszczyk <michal.leszczyk@performgroup.com>
2018-07-19 07:58:20 +0200

    fix: unit tests for counting

--- test/Greeter.js
+++ test/Greeter.js
@@ -5,13 +5,13 @@ describe('Greeter', () => {
 	it('greets one person', () => {
 		let greeter = new Greeter('mike')
 		expect(greeter.guests).to.deep.equal(['mike'])
-		expect(greeter.greeting).to.equal('Hello mike!')
+		expect(greeter.greeting).to.equal(`Hello mike! We've got 1 guests total!`)
 	})
 
 	it('greets two people', () => {
 		let greeter = new Greeter(['mike', 'matthew'])
 		expect(greeter.guests).to.deep.equal(['mike', 'matthew'])
-		expect(greeter.greeting).to.equal('Hello mike and matthew!')
+		expect(greeter.greeting).to.equal(`Hello mike and matthew! We've got 2 guests total!`)
 	})
 
 	it('adds more people and greets them', () => {
@@ -19,7 +19,7 @@ describe('Greeter', () => {
 		greeter.addGuest('john')
 
 		expect(greeter.guests).to.deep.equal(['mike', 'matthew', 'john'])
-		expect(greeter.greeting).to.equal('Hello mike, matthew and john!')
+		expect(greeter.greeting).to.equal(`Hello mike, matthew and john! We've got 3 guests total!`)
 	})
 })
 
